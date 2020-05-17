468b6477cfef999631eb5d5c43218d476e7c9d98
Artjom Jakovlev <edelmoedig@gmail.com>
2020-05-17 18:01:27 +0300

    Add views

---
+++ processes_views.sql
@@ -0,0 +1,4 @@
+CREATE OR REPLACE VIEW all_processes WITH (security_barrier) AS
+SELECT Process.process_id, Process.name, Process.description, Process.password IS NOT NULL AS has_password, Process_status_type.name AS current_status, format('%1$s %2$s', trim(Administrator.given_name), trim(Administrator.surname)) AS owner, Process.reg_time FROM Process_status_type INNER JOIN (Administrator INNER JOIN Process ON Administrator.administrator_id = Process.owner_id) ON Process_status_type.process_status_type_code = Process.process_status_type_code;
+
+COMMENT ON VIEW all_processes IS 'View shows basic information about all processes.'
