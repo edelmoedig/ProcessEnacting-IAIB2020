69d85ee7bbf923976e98fd33a49ac3de0908ce7c
Artjom Jakovlev <edelmoedig@gmail.com>
2020-05-19 23:03:49 +0300

    Use schema processes

--- processes_inserts.sql
+++ processes_inserts.sql
@@ -1,4 +1,4 @@
-INSERT INTO Process_status_type(process_status_type_code, name) VALUES (1, 'On hold');
-INSERT INTO Process_status_type(process_status_type_code, name) VALUES (2, 'Active');
-INSERT INTO Process_status_type(process_status_type_code, name) VALUES (3, 'Inactive');
-INSERT INTO Process_status_type(process_status_type_code, name) VALUES (4, 'Ended');
+INSERT INTO processes.Process_status_type(process_status_type_code, name) VALUES (1, 'On hold');
+INSERT INTO processes.Process_status_type(process_status_type_code, name) VALUES (2, 'Active');
+INSERT INTO processes.Process_status_type(process_status_type_code, name) VALUES (3, 'Inactive');
+INSERT INTO processes.Process_status_type(process_status_type_code, name) VALUES (4, 'Ended');
