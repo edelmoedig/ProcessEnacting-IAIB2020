3899bed7c3c179dba5bd948a26817255a7a059a4
Artjom Jakovlev <edelmoedig@gmail.com>
2020-05-19 16:23:05 +0300

    Standardize not_only_whitespace CHECK name

--- processes_tables.sql
+++ processes_tables.sql
@@ -56,8 +56,8 @@ CREATE TABLE Process
     password                 varchar(60)  NULL,
     CONSTRAINT PK_Process PRIMARY KEY (process_id),
     CONSTRAINT AK_Process_first_step_id UNIQUE (first_step_id),
-    CONSTRAINT CHK_Process_name_not_only_whitespaces CHECK (name !~ '^[[:space:]]*$'),
-    CONSTRAINT CHK_Process_description_not_only_whitespaces CHECK (description !~ '^[[:space:]]*$'),
+    CONSTRAINT CHK_Process_name_not_only_whitespace CHECK (name !~ '^[[:space:]]*$'),
+    CONSTRAINT CHK_Process_description_not_only_whitespace CHECK (description !~ '^[[:space:]]*$'),
     CONSTRAINT FK_Process_Process_status_type FOREIGN KEY (process_status_type_code) REFERENCES Process_status_type (process_status_type_code) ON DELETE No Action ON UPDATE Cascade,
     CONSTRAINT FK_Process_Administrator FOREIGN KEY (owner_id) REFERENCES Administrator (administrator_id) ON DELETE No Action ON UPDATE No Action
 ) WITH (fillfactor = 90);
@@ -173,7 +173,7 @@ CREATE TABLE Decision_table
     is_active         boolean      NOT NULL DEFAULT TRUE,
     reg_time          timestamp    NOT NULL DEFAULT LOCALTIMESTAMP(0),
     CONSTRAINT PK_Decision_table PRIMARY KEY (decision_table_id),
-    CONSTRAINT CHK_Decision_table_name_not_only_whitespaces CHECK (name !~ '^[[:space:]]*$'),
+    CONSTRAINT CHK_Decision_table_name_not_only_whitespace CHECK (name !~ '^[[:space:]]*$'),
     CONSTRAINT FK_Decision_table_Action FOREIGN KEY (action_id) REFERENCES Action (action_id) ON DELETE Cascade ON UPDATE No Action
 );
 CREATE INDEX IX_Decision_table_action_id ON Decision_table (action_id ASC);
@@ -189,8 +189,8 @@ CREATE TABLE Decision_table_entry
     CONSTRAINT PK_Decision_table_entry PRIMARY KEY (decision_table_entry_id),
     CONSTRAINT AK_decision_table_entry_seq_nr UNIQUE (decision_table_id, seq_nr),
     CONSTRAINT AK_decision_table_entry_condition UNIQUE (condition, decision_table_id),
-    CONSTRAINT CHK_Decision_table_entry_condition_not_only_whitespaces CHECK (condition !~ '^[[:space:]]*$'),
-    CONSTRAINT CHK_Decision_table_entry_action_not_only_whitespaces CHECK (action !~ '^[[:space:]]*$'),
+    CONSTRAINT CHK_Decision_table_entry_condition_not_only_whitespace CHECK (condition !~ '^[[:space:]]*$'),
+    CONSTRAINT CHK_Decision_table_entry_action_not_only_whitespace CHECK (action !~ '^[[:space:]]*$'),
     CONSTRAINT FK_Decision_table_entry_Decision_table FOREIGN KEY (decision_table_id) REFERENCES Decision_table (decision_table_id) ON DELETE Cascade ON UPDATE No Action
 );
 
